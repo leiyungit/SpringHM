563a94d94759784c6cd1e9327cbe8e96e8df40a3
leiyungit <744561873@qq.com>
2020-10-08 23:24:29 +0800

    SpringBoot first

---
+++ Boot/springboot_quick_idea/src/main/java/com/itcast/SpringbootQuickIdeaApplication.java
@@ -0,0 +1,13 @@
+package com.itcast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootQuickIdeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootQuickIdeaApplication.class, args);
+	}
+
+}
